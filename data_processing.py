1b2998ed93cc260d04e09871b2b80e24b511ec14
djwyen <mit.dereky@gmail.com>
2020-07-26 15:15:17 -0700

    Implement affricate joining and dataset splitting

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,17 @@
 import csv
 import os
 
+import random
+random.seed(42) # fix seed for testing purposes
+
 # code to parse the ielex data and build a dataset from an ancestral language and its descendants
 
 default_dataset_path = 'data/ielex.tsv' # Indo-European dataset
+
+# the header we will use in our custom .tsv data files; these are the properties each word will have. The two new columns, 'parsed_tokens' and 'split', will be populated with data we compute
+header = ['language', 'iso_code', 'gloss', 'global_id', 
+    'local_id', 'transcription', 'cognate_class', 'tokens', 'notes',
+    'parsed_tokens', 'split']
 
 old_norse_iso_code = 'non'
 # non: Old Norse, isl: Icelandic, fao: Faroese, swe: Swedish, dan: Danish, nor: Norwegian
@@ -109,15 +117,53 @@ def process_dataset(cognate_pair_dict):
         in performing our analysis and returning the new dictionary. Any new properties
         we add must also be named above in header as new columns.
     '''
+    # I aim to modify the dictionary in-place, as this preserves the internal order. This is useful for making it such that casting the dict to an iterator still causes it to return items in the same order.
+    # note(Derek): There's a more efficient way to do this that would only require looping through once overall instead of once per new column, but we don't have those speed concerns yet with the current dataset size.
 
     # add the parsed tokens column
+    def append_parsed_tokens(line):
+        iso_code = line[1]
+        tokens = line[7]
+        parsed_tokens = parse_tokens(tokens, iso_code)
+        line.append(parsed_tokens)
+
+    for global_id, lines in cognate_pair_dict.items():
+        parent_line, daughter_line = lines
+        append_parsed_tokens(parent_line)
+        append_parsed_tokens(daughter_line)
 
     # add the dataset split column
+    n_lines = len(cognate_pair_dict)
+    cognate_ids = list(cognate_pair_dict.keys())
+    # the split will be 80% training, 20% testing, and the training is 5-fold split for 5-fold validation
+    # in other words, the fractions are 5 sets of 4/25 and one set of 5/25 
+    random.shuffle(cognate_ids)
+
+    split_factor = int(round(n_lines * (4/25)))
+    print('split_factor', split_factor)
+
+    # create five splits of 4/25ths of the data each
+    for i in range(0, 5):
+        for cog_id in cognate_ids[i * split_factor: (i+1) * split_factor]:
+            parent_line, daughter_line = cognate_pair_dict[cog_id]
+            parent_line.append(str(i+1))
+            daughter_line.append(str(i+1))
+    
+    # the remaining 5/25ths goes to the test set
+    for cog_id in cognate_ids[5 * split_factor:]:
+        parent_line, daughter_line = cognate_pair_dict[cog_id]
+        parent_line.append('test')
+        daughter_line.append('test')
+
+    return cognate_pair_dict
+
 
 # an affricate map maps the stop part of an affricate to the fricative part
 general_affricate_map = {'t': 'ʃ', 'd': 'ʒ'} # for broad cross-linguistic purposes
+italian_affricate_map = general_affricate_map # geminated consonants can also create affricates, but we currently don't handle that
 
-affricate_maps = {'general': general_affricate_map} # maps an iso code to that language's affricate map
+affricate_maps = {'general': general_affricate_map, 
+    'ita': italian_affricate_map} # maps an iso code to that language's affricate map
 
 def parse_tokens(word, language='general'):
     '''
@@ -133,7 +179,10 @@ def parse_tokens(word, language='general'):
 
     returns: str, the retokenized word, tokens are space-separated
     '''
-    affricate_map = affricate_maps[language]
+    if language in affricate_maps:
+        affricate_map = affricate_maps[language]
+    else:
+        affricate_map = affricate_maps['general']
 
     tokens = word.split(' ')
     new_tokens = []
@@ -165,11 +214,18 @@ def save_dataset(cognate_pair_dict, output_dir=None):
     # we extract an arbitrary line from the dictionary to obtain the parent and daughter iso codes
     parent_line, daughter_line = next(iter(cognate_pair_dict.values())) # popping from an iterator is best way of doing this: it uses the least space since it doesn't load the whole dictionary
 
+    # this function can fill in columns that are empty with blank data, but it can't discard columns if the header is underspecified
+    assert len(header) >= len(parent_line) == len(daughter_line)
+    # if the cognate dictionary hasn't been processed to populate the new column values for each cognate, we must append blank entries to the lines so that the resulting .tsv has entries in all columns
+    # note that I assume any new column properties we add are added to the end of the header, on the right side
+    n_missing_columns = len(header) - len(parent_line)
+    pad = [''] * n_missing_columns
+
     if output_dir is None: # generate default directory name
         parent_language, daughter_language = parent_line[0], daughter_line[0]
         output_dir = parent_language + '-' + daughter_language + ' Cognates'
         output_dir = output_dir.title() # format to make the folder easier to read
-    # note (Derek): it occurs to me that the above method of extracting the language names can potentially cause an undesirable bug. Since multiple distinct doculects could be under the same iso code but have a different listed language name, it is theoretically possible that the same cognate dictionary could be saved to a different directory on different runs, if the line that is popped from the iterator changes. If it is true that Python dictionaries have an internal order in python 3, however, then the line popped from the iterator is deterministic (for a given dataset) and this is not a problem.
+    # note (Derek): it occurs to me that the above method of extracting the language names can potentially cause an undesirable bug. Since multiple distinct doculects could be under the same iso code but have a different listed language name, it is theoretically possible that the same cognate dictionary could be saved to a different directory on different runs, if the line that is popped from the iterator changes. If it is true that Python dictionaries have an internal order in python 3, however, then the line popped from the iterator is deterministic (for a given dataset) and this is not a problem. We are shuffling the dataset for the splits, though, so this is potentially a problem again...
     
     output_dir = os.path.join('data', output_dir)
     if not os.path.exists(output_dir):
@@ -183,16 +239,29 @@ def save_dataset(cognate_pair_dict, output_dir=None):
         writer_p = csv.writer(f_p, delimiter='\t')
         writer_d = csv.writer(f_d, delimiter='\t')
 
-        header = ['language', 'iso_code', 'gloss', 'global_id', 
-            'local_id', 'transcription', 'cognate_class', 'tokens', 'notes']
         writer_p.writerow(header)
         writer_d.writerow(header)
 
         for k in cognate_pair_dict.keys():
             parent_line, daughter_line = cognate_pair_dict[k]
-            writer_p.writerow(parent_line)
-            writer_d.writerow(daughter_line)
+            # the columns are padded with blank entries if they are missing any data
+            writer_p.writerow(parent_line + pad)
+            writer_d.writerow(daughter_line + pad)
     
-save_dataset(romance_cognate_pair_dicts['fra'])
 
-save_dataset(filter_daughter('lat', 'por'))
+# def save_processed_dataset(cognate_pair_dict, output_dir=None):
+
+
+# save_dataset(romance_cognate_pair_dicts['fra'])
+
+test_dict = process_dataset(filter_daughter('lat', 'ita'))
+split_counts = {}
+
+for parent, daughter in test_dict.values():
+    if parent[10] not in split_counts:
+        split_counts[parent[10]] = 0
+    split_counts[parent[10]] += 1
+
+print(split_counts)
+
+save_dataset(test_dict)
