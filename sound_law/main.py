d792804b64fe8b22d0fcbb5a18721cb4776ea198
j-luo93 <j_luo@mit.edu>
2020-08-02 23:20:54 -0400

    show_args call in main

--- sound_law/main.py
+++ sound_law/main.py
@@ -1,4 +1,4 @@
-from dev_misc import initiate
+from dev_misc import initiate, parse_args, show_args
 from dev_misc.devlib.named_tensor import patch_named_tensors
 from sound_law.train.manager import OnePairManager
 
@@ -11,4 +11,6 @@ def run():
 if __name__ == '__main__':
     initiate(logger=True, log_level=True, gpus=True, random_seed=True, commit_id=True, log_dir=True)
     patch_named_tensors()
+    parse_args()
+    show_args()
     run()
