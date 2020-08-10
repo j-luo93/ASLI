39ab0899ec7436feef718d2842654c591ee438c6
j-luo93 <j_luo@mit.edu>
2020-08-10 14:51:07 -0400

    config file

---
+++ sound_law/config.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from typing import Tuple
+
+from dev_misc.arglib import Registry
+
+reg = Registry('config')
+
+
+@reg
+class ZSLatIta:  # "ZS" stands for zero-shot.
+    data_path: Path = Path('data/wikt')
+    src_lang: str = 'lat'
+    tgt_lang: str = 'ita'
+    check_interval: int = 100
+    num_steps: int = 10000
+    input_format: str = 'wikt'
+    eval_interval: int = 1000
+    control_mode: str = 'none'
+    train_tgt_langs: Tuple[str] = ('ron', 'fra', 'spa', 'por')
+    task: str = 'one_to_many'
+    lang_emb_mode: str = 'mean'
--- sound_law/main.py
+++ sound_law/main.py
@@ -1,5 +1,6 @@
 from dev_misc import add_argument, g, initiate, parse_args, show_args
 from dev_misc.devlib.named_tensor import patch_named_tensors
+from sound_law.config import reg
 from sound_law.train.manager import OnePairManager, OneToManyManager
 
 add_argument('task', dtype=str, default='one_pair', choices=['one_pair', 'one_to_many'], msg='Which task to execute.')
@@ -14,7 +15,7 @@ def run():
 
 
 if __name__ == '__main__':
-    initiate(logger=True, log_level=True, gpus=True, random_seed=True, commit_id=True, log_dir=True)
+    initiate(reg, logger=True, log_level=True, gpus=True, random_seed=True, commit_id=True, log_dir=True)
     patch_named_tensors()
     parse_args()
     show_args()
