f1431c0bae4fd504ff4f89d9a21b572ec7abea31
j-luo93 <j_luo@mit.edu>
2020-08-10 22:42:12 -0400

    option to save model

--- sound_law/train/trainer.py
+++ sound_law/train/trainer.py
@@ -1,5 +1,6 @@
 import logging
 
+import torch
 from torch.nn.utils import clip_grad_norm_
 
 from dev_misc import add_argument, g
@@ -11,12 +12,19 @@ from sound_law.data.data_loader import OnePairDataLoader
 class Trainer(BaseTrainer):
 
     add_argument('num_steps', default=1000, dtype=int, msg='Number of steps for training.')
+    add_argument('save_model', dtype=bool, default=True, msg='Flag to save model.')
 
     def add_trackables(self):
         self.tracker.add_count_trackable('step', g.num_steps)
 
     def save(self, eval_metrics: Metrics):
-        logging.info('Not saving anything.')
+        if g.save_model:
+            path = g.log_dir / 'saves' / f'model.{self.stage}.pth'
+            path.parent.mkdir(exist_ok=True, parents=True)
+            torch.save(self.model.state_dict(), path)
+            logging.info(f'Model saved to {path}.')
+        else:
+            logging.info('No model is saved.')
 
     def train_one_step(self, dl: OnePairDataLoader) -> Metrics:
         batch = dl.get_next_batch()
