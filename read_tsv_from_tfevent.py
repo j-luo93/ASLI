6cf5edacaabec4d03850346e2a8285fdf9106e44
djwyen <mit.dereky@gmail.com>
2020-08-18 10:30:18 -0700

    Finish tfevent to tsv script

--- read_tsv_from_tfevent.py
+++ read_tsv_from_tfevent.py
@@ -5,8 +5,6 @@ import argparse
 
 from tensorflow.python.summary.summary_iterator import summary_iterator
 
-# print(next(summary_iterator('log/2020-08-17/ZSLatItaPhono/02-12-14/events.out.tfevents.1597644744.rosetta3.4547.0')))
-
 # the summary_iterator returns objects with the following form:
 # wall_time: float
 # step: int
@@ -16,19 +14,17 @@ from tensorflow.python.summary.summary_iterator import summary_iterator
 #       simple_value: float
 #   }
 # }
-sample_path = 'log/2020-08-17/ZSLatItaPhono/02-12-14/events.out.tfevents.1597644744.rosetta3.4547.0'
-# for e in summary_iterator(sample_path):
-#     if e.step == 10000:
-#         print(e.summary.value)
 
 def get_event_file_paths(directory):
     '''returns filepaths to all tfevents files in a certain directory'''
-    return glob.glob(os.path.join(directory, '*', 'event*'))
+    return glob.glob(os.path.join(directory, '*', '*', 'event*'))
 
 def read_event_file(path):
     header = ['step', 'tag', 'score']
 
-    file_name = os.path.basename(path) + '.tsv' # this loses info about the full path; alternative below
+    if not os.path.exists('processed_log'):
+        os.mkdir('processed_log')
+    file_name = os.path.join('processed_log', os.path.basename(path) + '.tsv') # this loses info about the full path; alternative below
     # file_name = path.replace('/', '_') + '.tsv' # preserves all info about the full path but creates very long filenames
     # maybe a smarter alternative would be just to have a two-line header, with the first line being the full filepath. This is slightly unconventional but it's smart here.
 
@@ -42,13 +38,21 @@ def read_event_file(path):
                 line = [e.step, v.tag, v.simple_value]
                 writer.writerow(line)
 
-read_event_file(sample_path)
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="export a tfevents file to a tsv")
 
-    parser.add_argument('--data_path', required=True, help='folder to export tfevents files from')
-    # parser.add_argument('--step', default=10000, nargs='?', help='step count to collect data from')
-    parser.add_argument('--output_file', required=False)
-    
+    parser.add_argument('--data_path', required=True, help='date-level directory to export tfevents files from (eg log/2020-08-18)')
+    parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
+
+    args = parser.parse_args()
+
+    paths = get_event_file_paths(args.data_path)
+    if args.verbose:
+        print("Reading the following tfevent files:")
+    for path in paths:
+        if args.verbose:
+            print('\t' + path)
+        read_event_file(path)
+    if args.verbose:
+        print("Saved .tsv files under processed_log/")
