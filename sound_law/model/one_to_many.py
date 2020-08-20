b4368442ec7db3e881e87cdc6b2f050359eb5fdc
djwyen <mit.dereky@gmail.com>
2020-08-20 13:10:54 -0700

    Fix bug with encoder initialization in OneToMany

--- sound_law/model/one_to_many.py
+++ sound_law/model/one_to_many.py
@@ -40,30 +40,3 @@ class OneToManyModel(OnePairModel):
                                               max_length=max_length,
                                               lang_emb=lang_emb)
         return log_probs, almt_distrs
-
-
-class CnnEncoderOneToManyModel(CnnEncoderOnePairModel):
-    
-    def __init__(self, num_src_chars: int, num_tgt_chars: int, num_tgt_langs: int, unseen_idx: int,
-                 lang2id: Optional[Dict[str, int]] = None,
-                 phono_feat_mat: Optional[LT] = None,
-                 special_ids: Optional[Sequence[int]] = None):
-        super().__init__(num_src_chars, num_tgt_chars, lang2id=lang2id,
-                         phono_feat_mat=phono_feat_mat, special_ids=special_ids)
-        self.lang_emb = LanguageEmbedding(num_tgt_langs, g.char_emb_size,
-                                          unseen_idx=unseen_idx,
-                                          lang2id=lang2id,
-                                          mode=g.lang_emb_mode,
-                                          dropout=g.dropout)
-    
-    def forward(self, batch: OnePairBatch, use_target: bool = True, max_length: int = None) -> Tuple[FT, FT]:
-        src_emb, output, state = self.encoder(batch.src_seqs.ids, batch.src_seqs.lengths)
-        lang_emb = self.lang_emb(batch.tgt_seqs.lang_id)
-        target = batch.tgt_seqs.ids if use_target else None
-        log_probs, almt_distrs = self.decoder(SOT_ID, src_emb,
-                                              output, batch.src_seqs.paddings,
-                                              target=target,
-                                              max_length=max_length,
-                                              lang_emb=lang_emb)
-        return log_probs, almt_distrs
-
