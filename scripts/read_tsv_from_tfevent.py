e8e89f1ed0474920c6ffb2a140101385cf435205
j-luo93 <j_luo@mit.edu>
2020-09-21 11:33:34 -0400

    event-to-tsv script now discovers event files using the ** glob syntax from pathlib; tsv files renamed, added directory info

--- scripts/read_tsv_from_tfevent.py
+++ scripts/read_tsv_from_tfevent.py
@@ -1,7 +1,8 @@
+import argparse
+import csv
 import glob
 import os
-import csv
-import argparse
+from pathlib import Path
 
 from tensorflow.python.summary.summary_iterator import summary_iterator
 
@@ -15,9 +16,12 @@ from tensorflow.python.summary.summary_iterator import summary_iterator
 #   }
 # }
 
+
 def get_event_file_paths(directory):
     '''returns filepaths to all tfevents files in a certain directory'''
-    return glob.glob(os.path.join(directory, '*', '*', 'event*'))
+    return map(str, Path(directory).glob('**/event*'))
+    # return glob.glob(os.path.join(directory, '*', '*', 'event*'))
+
 
 def read_event_file(path):
     header = ['step', 'tag', 'value']
@@ -25,13 +29,15 @@ def read_event_file(path):
 
     if not os.path.exists('processed_log'):
         os.mkdir('processed_log')
-    file_name = os.path.join('processed_log', os.path.basename(path) + '.tsv') # this loses info about the full path; alternative below
+    # this loses info about the full path; alternative below
+    name = path.replace("/", '__')
+    file_name = os.path.join('processed_log', name + '.tsv')
     # file_name = path.replace('/', '_') + '.tsv' # preserves all info about the full path but creates very long filenames
     # maybe a smarter alternative would be just to have a two-line header, with the first line being the full filepath. This is slightly unconventional but it's smart here.
 
     with open(file_name, 'w') as f:
         writer = csv.writer(f, delimiter='\t')
-        
+
         writer.writerow(header)
         for e in summary_iterator(path):
             # there's only one entry in each Event, but this is the only way I have found to extract the Value
@@ -43,7 +49,8 @@ def read_event_file(path):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="export a tfevents file to a tsv")
 
-    parser.add_argument('--data_path', required=True, help='date-level directory to export tfevents files from (eg log/2020-08-18)')
+    parser.add_argument('--data_path', required=True,
+                        help='date-level directory to export tfevents files from (eg log/2020-08-18)')
     parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
 
     args = parser.parse_args()
