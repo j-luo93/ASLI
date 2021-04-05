0add64fe114133418982ae62ac70e79993ca1493
j-luo93 <j_luo@mit.edu>
2021-04-05 17:50:46 -0400

    ilp.py runs

--- sound_law/evaluate/ilp.py
+++ sound_law/evaluate/ilp.py
@@ -1,26 +1,27 @@
 from __future__ import annotations
 
+import bisect
 import logging
 import pickle
+import random
 import re
 from dataclasses import dataclass, field
-from typing import ClassVar, List, Set, Dict, Optional, Union
+from typing import ClassVar, Dict, List, Optional, Set, Union
+
 import pandas as pd
-
 from ortools.linear_solver import pywraplp
-import random
-import bisect
 
-from dev_misc import add_argument, g
-from sound_law.main import setup
-# from sound_law.data.alphabet import Alphabet
-from sound_law.rl.action import SoundChangeAction
 # from sound_law.rl.env import ToyEnv
 # from sound_law.rl.mcts_cpp import \
 #     PyNull_abc  # pylint: disable=no-name-in-module
 # from sound_law.rl.trajectory import VocabState
 import sound_law.rl.rule as rule
+from dev_misc import add_argument, g
+from sound_law.main import setup
+# from sound_law.data.alphabet import Alphabet
+from sound_law.rl.action import SoundChangeAction
 from sound_law.rl.rule import HandwrittenRule
+
 
 class ToyEnv():
 
@@ -64,13 +65,12 @@ def match_rulesets(gold: List[List[SoundChangeAction]],
                    k_matches: int = 10) -> List[List[Int, List[Int]]]:
     '''Finds the optimal matching of rule blocks in the gold ruleset to 0, 1, or 2 rules in the candidate ruleset. Frames the problem as an integer linear program. Returns a list of tuples with the matching.'''
 
-    solver = pywraplp.Solver.CreateSolver('SCIP') # TODO investigate other solvers
+    solver = pywraplp.Solver.CreateSolver('SCIP')  # TODO investigate other solvers
     # form the different variables in this ILP
     # this dict maps strings to pointers to the variable that string represents. Makes things much more readable.
     v = {}
     # this dict maps rules/blocks to their individual constraint: eg 'gold_0' for the constraint for the 0th gold block, or 'cand_3' for the 3th candidate rule.
     c = {}
-
 
     # initialize constraints for all blocks/rules
     # constraint is of form a_i0 + ... + a_im + b_i(01) + ... <= 1
@@ -84,15 +84,17 @@ def match_rulesets(gold: List[List[SoundChangeAction]],
     # finally, this matching constraint forces the model to match at least some of the rules (otherwise it would just match no rules to vacuously achieve a minimum objective of 0)
     # it stipulates that the sum of all variables must be >= some minimum match number
     # by the handshake lemma, only a constraint needs to be placed on gold — this implies some amount of matching with candidate
-    c['min_match'] = solver.Constraint(0, 1) # we will update the actual bounds later based on how many gold blocks are actually active in this vocab, i.e. how many gold blocks actually apply to one or more words in the vocab
-    number_active_gold_blocks = 0 # counts the number of gold blocks eligible for matching, ie rules that actually change words
+    # we will update the actual bounds later based on how many gold blocks are actually active in this vocab, i.e. how many gold blocks actually apply to one or more words in the vocab
+    c['min_match'] = solver.Constraint(0, 1)
+    number_active_gold_blocks = 0  # counts the number of gold blocks eligible for matching, ie rules that actually change words
 
     curr_state = env.start
     objective = solver.Objective()
     for i in range(len(gold)):
         # as an optimization, we only create variables for the best k_matches that a given gold block has with collections of rules in candidate. We assume that matchings with higher cost would never be chosen anyway and won't affect the solution, so they can just be excluded from the linear program.
         highest_cost = None
-        paired_costs = [] # entries are of form (varname, i, [j...k], cost) — ie the variable pairing i with rules [j...k] has cost coefficient cost. Costs are in increasing order.
+        # entries are of form (varname, i, [j...k], cost) — ie the variable pairing i with rules [j...k] has cost coefficient cost. Costs are in increasing order.
+        paired_costs = []
         block = gold[i]
         # print('block', i, block)
         try:
@@ -111,7 +113,6 @@ def match_rulesets(gold: List[List[SoundChangeAction]],
                 # print('cand:', j, rule)
 
                 # TODO(djwyen) add try/excepts to each other application of apply_action
-                breakpoint()  # BREAKPOINT(j_luo)
                 try:
                     cand_state = env.apply_action(curr_state, rule)
                 except RuntimeError:
@@ -126,12 +127,12 @@ def match_rulesets(gold: List[List[SoundChangeAction]],
                     if len(paired_costs) < k_matches or cost < highest_cost:
                         if len(paired_costs) == k_matches:
                             del paired_costs[-1]
-                        bisect.insort_left(paired_costs, new_tuple) # insert in sorted order
-                        highest_cost = paired_costs[-1][3] # update costs
+                        bisect.insort_left(paired_costs, new_tuple)  # insert in sorted order
+                        highest_cost = paired_costs[-1][3]  # update costs
 
             for j in range(len(cand)):
                 rule1 = cand[j]
-                for k in range(j+1, len(cand)):
+                for k in range(j + 1, len(cand)):
                     rule2 = cand[k]
                     b_var_name = 'b_' + str(i) + ',(' + str(j) + ',' + str(k) + ')'
 
@@ -141,7 +142,7 @@ def match_rulesets(gold: List[List[SoundChangeAction]],
                         pass
                     else:
                         cost = env.get_state_edit_dist(gold_state, cand_state)
-                        new_tuple = (b_var_name, i, [j,k], cost)
+                        new_tuple = (b_var_name, i, [j, k], cost)
 
                         if len(paired_costs) < k_matches or cost < highest_cost:
                             if len(paired_costs) == k_matches:
@@ -151,9 +152,9 @@ def match_rulesets(gold: List[List[SoundChangeAction]],
 
             for j in range(len(cand)):
                 rule1 = cand[j]
-                for k in range(j+1, len(cand)):
+                for k in range(j + 1, len(cand)):
                     rule2 = cand[k]
-                    for l in range(k+1, len(cand)):
+                    for l in range(k + 1, len(cand)):
                         rule3 = cand[l]
                         c_var_name = 'c_' + str(i) + ',(' + str(j) + ',' + str(k) + ',' + str(l) + ')'
 
@@ -163,7 +164,7 @@ def match_rulesets(gold: List[List[SoundChangeAction]],
                             pass
                         else:
                             cost = env.get_state_edit_dist(gold_state, cand_state)
-                            new_tuple = (c_var_name, i, [j,k,l], cost)
+                            new_tuple = (c_var_name, i, [j, k, l], cost)
 
                             if len(paired_costs) < k_matches or cost < highest_cost:
                                 if len(paired_costs) == k_matches:
@@ -198,16 +199,16 @@ def match_rulesets(gold: List[List[SoundChangeAction]],
     # interpret solution as a matching, returning a list pairing indices of blocks in gold to a list of indices of matched rules in cand
     matching = []
     for name, var in v.items():
-        if var.solution_value(): # ie if this variable was set to 1
+        if var.solution_value():  # ie if this variable was set to 1
             # print('%s = %d' % (var.name(), var.solution_value()))
 
             # process the variable name to extract the IDs of the matched rules
             # example name: b_16,(20,24,27)
-            id_half = name.split('_')[1] # name: 16,(20,24,27)
-            gold_half, cand_half = id_half.split('(') # 16, ; 20,24,27)
-            gold_var = int(gold_half[:-1]) # remove the comma and turn into an int
-            cand_vars = cand_half[:-1].split(',') # remove the right paren and split on the commas
-            cand_vars = [int(x) for x in cand_vars] # make the numbers into ints
+            id_half = name.split('_')[1]  # name: 16,(20,24,27)
+            gold_half, cand_half = id_half.split('(')  # 16, ; 20,24,27)
+            gold_var = int(gold_half[:-1])  # remove the comma and turn into an int
+            cand_vars = cand_half[:-1].split(',')  # remove the right paren and split on the commas
+            cand_vars = [int(x) for x in cand_vars]  # make the numbers into ints
 
             match = [gold_var, cand_vars]
             matching.append(match)
@@ -223,7 +224,6 @@ def match_rulesets(gold: List[List[SoundChangeAction]],
                 print('with dist', str(cost))
 
     return matching
-
 
 
 if __name__ == "__main__":
